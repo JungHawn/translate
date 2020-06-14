573476dc42d49b4c8183c78d70e5b8536030d5e0
You Jung Hawn <u_egg@naver.com>
2020-06-14 14:05:24 +0900

    Update papago.py
    + 크롤링 + GUI

--- papago.py
+++ papago.py
@@ -2,6 +2,10 @@ import os
 import sys
 import urllib.request
 import json,pprint
+import urllib.parse
+from bs4 import BeautifulSoup
+from tkinter import *
+from tkinter import messagebox
 
 def translate():
     # 번역할 txt파일 내용 불러와서 읽기
@@ -48,4 +52,68 @@ def translate():
     else:
         print("Error Code:" + rescode)
 
-translate()
+def crawling():
+    
+    with open('source.txt', 'r', encoding='utf8') as f:
+        date = f.read()
+
+    plusUrl = urllib.parse.quote_plus(date)
+
+    pageNum = 1
+    count = 1
+
+    i = 1 #1페이지로 고정
+
+    sys.stdout = open('output.txt', 'a', encoding='UTF-8')
+
+    lastPage = int(i) * 10 - 9
+    while pageNum < lastPage + 1:
+        url = f'https://search.naver.com/search.naver?date_from=&date_option=0&date_to=&dup_remove=1&nso=&post_blogurl=&post_blogurl_without=&query={plusUrl}&sm=tab_pge&srchby=all&st=sim&where=post&start={pageNum}'
+
+        html = urllib.request.urlopen(url).read()
+        soup = BeautifulSoup(html, 'html.parser')
+
+        title = soup.find_all(class_='sh_blog_title')
+
+        print(f'-----{plusUrl}의 {count}페이지 결과 입니다.-----')
+        for i in title:
+            print(i.attrs['title'])
+            print(i.attrs['href'])
+
+        print()
+
+        pageNum += 10
+        count += 1
+        messagebox.showinfo("크롤링", "크롤링 되었습니다.")
+
+def close():
+    messagebox.showinfo("종료","프로그램을 종료합니다.")
+    quit()
+
+def main():
+    window = Tk()
+    window.title("메뉴선택")#GUI창 이름변경
+    window.geometry("640x400+100+100")#크기 설정
+    window.resizable(0,0)
+    photo = PhotoImage(file="CBNU.png");
+    labe2 = Label(window, image=photo);
+    labe3 = Label(window, text='글씨 인식 프로그램', font=(30))
+
+    # 호출
+    b1 = Button(window, text="1. 번역", width=15,height=5,compound="c",command=translate)
+    b2 = Button(window, text="2. 웹 크롤링",width=15,height=5,compound="c", command=crawling)
+    b3 = Button(window, text="3. 학교 위치 정보", width=15,height=5, compound="c", command=crawling)
+    b4 = Button(window, text="4. 캘린더", width=15,height=5, compound="c", command=close)
+    b5 = Button(window, text="5. 종료",width=15,height=5,compound="c", command=close)
+
+    labe2.pack()
+    labe3.pack()
+    b1.pack(side=LEFT, padx=10)
+    b2.pack(side=LEFT, padx=10)
+    b3.pack(side=LEFT, padx=10)
+    b4.pack(side=LEFT, padx=10)
+    b5.pack(side=LEFT, padx=10)
+
+    window.mainloop()
+
+main()
